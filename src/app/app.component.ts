20ef4df4fbb12446ad7fd0a2cbc5748f9cef16da
Momoz64100 <mickael.amorich@gmail.com>
2019-05-15 11:03:24 +0200

    test modal + modifs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { IPortfolio, IPortfolioImage, IPortfolioFolder } from './portfolio';
 
 @Component({
   selector: 'app-root',
@@ -10,41 +9,6 @@ export class AppComponent {
 
 
   ngOnInit() { 
-    this.buildPortoflio();
-  }
-
-  buildPortoflio()
-  {
-    var portfolioFolders: IPortfolioFolder[];
-
-    var avantProjetImg: IPortfolioImage[];
-    for (let i = 0; i < 16; i++) {
-      avantProjetImg.push({
-        id: i + 1,
-        extension: "jpg"
-      })      
-    }
-    portfolioFolders.push({
-      title: "avant-projet",
-      images: avantProjetImg
-    })    
-
-    // var insertion3d: IPortfolioImage[] = [];
-    // for (let i = 0; i < 5; i++) {
-    //   insertion3d.push({
-    //     id: i + 1,
-    //     extension: "jpg"
-    //   })      
-    // } 
-    // portfolioFolders.push({
-    //   title: "insertion3d",
-    //   images: insertion3d
-    // }) 
-
     
-    // var portfolio: IPortfolio = {
-    //   folder: portfolioFolders
-    // }
-    console.log("test");
   }
 }
